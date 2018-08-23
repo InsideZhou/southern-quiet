04c2155f4266e07aa6bf8a7c1d2007be01468079
周剑 <zhoujian@asiainfo.com>
2018-08-23 21:29:56 +0800

    添加debug日志。

--- framework/src/main/java/com/ai/southernquiet/util/SerializationUtils.java
+++ framework/src/main/java/com/ai/southernquiet/util/SerializationUtils.java
@@ -1,9 +1,22 @@
 package com.ai.southernquiet.util;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.nustaq.serialization.FSTConfiguration;
 
 public abstract class SerializationUtils {
-    private static FSTConfiguration fstConf = FSTConfiguration.createDefaultConfiguration();
+    private final static Log log = LogFactory.getLog(SerializationUtils.class);
+    private final static FSTConfiguration fstConf;
+
+    static {
+        try {
+            fstConf = FSTConfiguration.createDefaultConfiguration();
+        }
+        catch (Exception e) {
+            log.debug("静态初始化异常", e);
+            throw e;
+        }
+    }
 
     /**
      * Serialize the given object to a byte array.
