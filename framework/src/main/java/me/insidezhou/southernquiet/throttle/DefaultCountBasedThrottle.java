06c9c144b4f8164972b653f64e7a1372ede77b26
Inside <inside@insidezhou.me>
2020-04-14 09:49:51 +0800

    移除与接口定义不一致的实现注释。

--- framework/src/main/java/me/insidezhou/southernquiet/throttle/DefaultCountBasedThrottle.java
+++ framework/src/main/java/me/insidezhou/southernquiet/throttle/DefaultCountBasedThrottle.java
@@ -11,11 +11,6 @@ public class DefaultCountBasedThrottle implements Throttle {
         if (counter++ >= threshold) return reset();
 
         return false;
-//      下面第一行是counter，后几行是threshold分别为1、2、3时，节流器在第几个counter打开
-//        1 2 3 4 5 6 7 8 9 10
-//        1   3   5   7   9       threshold=1
-//        1     4     7     10    threshold=2
-//        1       5       9       threshold=3
     }
 
     private boolean reset() {
