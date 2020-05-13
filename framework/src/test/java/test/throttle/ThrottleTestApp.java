d2d8bec2c873616ae4693e555abc31e85faa142c
Inside <inside@insidezhou.me>
2020-05-13 19:28:19 +0800

    补充遗漏的文件。

---
+++ framework/src/test/java/test/throttle/ThrottleTestApp.java
@@ -0,0 +1,37 @@
+package test.throttle;
+
+import me.insidezhou.southernquiet.FrameworkAutoConfiguration;
+import me.insidezhou.southernquiet.throttle.annotation.ThrottledSchedule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
+@ImportAutoConfiguration({FrameworkAutoConfiguration.class})
+public class ThrottleTestApp {
+    private final static Logger log = LoggerFactory.getLogger(ThrottleTestApp.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThrottleTestApp.class, args);
+    }
+
+    @Bean
+    public ScheduledThrottleBean scheduledThrottleBean() {
+        return new ScheduledThrottleBean();
+    }
+
+    public static class ScheduledThrottleBean {
+        private final long begin = System.currentTimeMillis();
+
+        @ThrottledSchedule(fixedRate = 10, threshold = 3, timeUnit = TimeUnit.SECONDS)
+        public void scheduledThrottleMethod() {
+            log.info("scheduledThrottleMethod working ...\tbegin={}, elapsed={}", begin, Duration.ofMillis(System.currentTimeMillis() - begin));
+        }
+    }
+}
