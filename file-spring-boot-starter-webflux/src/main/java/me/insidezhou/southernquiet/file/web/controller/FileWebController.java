eeb082b51827d215f6e5e3ec55d12b247b5898fa
Inside <inside@insidezhou.me>
2020-08-05 16:42:12 +0800

    文件服务新增etag支持。

--- file-spring-boot-starter-webflux/src/main/java/me/insidezhou/southernquiet/file/web/controller/FileWebController.java
+++ file-spring-boot-starter-webflux/src/main/java/me/insidezhou/southernquiet/file/web/controller/FileWebController.java
@@ -5,6 +5,7 @@ import me.insidezhou.southernquiet.file.web.exception.NotFoundException;
 import me.insidezhou.southernquiet.file.web.model.FileInfo;
 import me.insidezhou.southernquiet.file.web.model.ImageScale;
 import me.insidezhou.southernquiet.filesystem.FileSystem;
+import me.insidezhou.southernquiet.filesystem.InvalidFileException;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.tika.Tika;
@@ -13,7 +14,9 @@ import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.core.io.buffer.DefaultDataBufferFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.http.codec.multipart.Part;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -44,11 +47,11 @@ public class FileWebController {
             .collect(Collectors.joining(FileSystem.PATH_SEPARATOR_STRING)) + FileSystem.PATH_SEPARATOR_STRING + filename;
     }
 
-    private final Tika tika = new Tika();
+    protected final Tika tika = new Tika();
 
-    private final FileSystem fileSystem;
-    private final String contextPath;
-    private final FileWebFluxAutoConfiguration.Properties fileWebProperties;
+    protected final FileSystem fileSystem;
+    protected final String contextPath;
+    protected final FileWebFluxAutoConfiguration.Properties fileWebProperties;
 
     public FileWebController(FileSystem fileSystem,
                              FileWebFluxAutoConfiguration.Properties fileWebProperties,
@@ -150,95 +153,117 @@ public class FileWebController {
             });
     }
 
-    public Flux<DataBuffer> file(String id, ServerHttpResponse response) {
+    public ResponseEntity<Flux<DataBuffer>> file(String id, ServerHttpRequest request) throws IOException {
         String path = getFilePath(id);
-
         if (!fileSystem.exists(path)) throw new NotFoundException();
 
-        return DataBufferUtils.readInputStream(
-            () -> {
-                InputStream resultStream;
-                try (InputStream inputStream = fileSystem.openReadStream(path)) {
-                    resultStream = new ByteArrayInputStream(StreamUtils.copyToByteArray(inputStream));
-                }
+        if (!request.getHeaders().getIfNoneMatch().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_MODIFIED).build();
+        }
 
-                response.getHeaders().set("Content-Type", tika.detect(resultStream));
-                resultStream.reset();
+        InputStream resultStream;
+        try (InputStream inputStream = fileSystem.openReadStream(path)) {
+            resultStream = new ByteArrayInputStream(StreamUtils.copyToByteArray(inputStream));
+        }
+        catch (InvalidFileException e) {
+            throw new IOException(e);
+        }
 
-                response.getHeaders().set("Content-Length", String.valueOf(resultStream.available()));
-                return resultStream;
-            },
-            new DefaultDataBufferFactory(),
-            (int) fileWebProperties.getBufferSize().toBytes());
+        String contentType = tika.detect(resultStream);
+        resultStream.reset();
+
+        final InputStream finalResultStream = resultStream;
+
+        return ResponseEntity.ok()
+            .contentLength(resultStream.available())
+            .contentType(MediaType.parseMediaType(contentType))
+            .eTag(id)
+            .body(DataBufferUtils.readInputStream(
+                () -> finalResultStream,
+                new DefaultDataBufferFactory(),
+                (int) fileWebProperties.getBufferSize().toBytes()));
     }
 
-    public Mono<String> base64file(String id, ServerHttpResponse response) {
+    public ResponseEntity<Mono<String>> base64file(String id, ServerHttpRequest request) throws IOException {
         String path = getFilePath(id);
-
         if (!fileSystem.exists(path)) throw new NotFoundException();
 
-        return Mono.create(sink -> {
-            response.getHeaders().set("Content-Type", MediaType.TEXT_PLAIN_VALUE);
+        if (!request.getHeaders().getIfNoneMatch().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_MODIFIED).build();
+        }
 
-            try (InputStream inputStream = fileSystem.openReadStream(path)) {
-                String base64 = Base64.encodeBase64String(StreamUtils.copyToByteArray(inputStream));
-                response.getHeaders().set("Content-Length", String.valueOf(base64.length()));
-                sink.success(base64);
+        String base64;
+        try (InputStream inputStream = fileSystem.openReadStream(path)) {
+            base64 = Base64.encodeBase64String(StreamUtils.copyToByteArray(inputStream));
+        }
+        catch (InvalidFileException e) {
+            throw new IOException(e);
+        }
+
+        return ResponseEntity.ok()
+            .contentLength(base64.length())
+            .contentType(MediaType.TEXT_PLAIN)
+            .eTag(id)
+            .body(Mono.create(sink -> sink.success(base64)));
+    }
+
+    public ResponseEntity<Flux<DataBuffer>> image(String id, ImageScale scale, ServerHttpRequest request, ServerHttpResponse response) throws IOException {
+        return image(id, scale, Scalr.Method.AUTOMATIC, request, response);
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    protected ResponseEntity<Flux<DataBuffer>> image(String id, ImageScale scale, Scalr.Method scaleMethod, ServerHttpRequest request, ServerHttpResponse response) throws IOException {
+        String path = getFilePath(id);
+        if (!fileSystem.exists(path)) throw new NotFoundException();
+
+        if (!request.getHeaders().getIfNoneMatch().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_MODIFIED).build();
+        }
+
+        InputStream resultStream;
+        try (InputStream inputStream = fileSystem.openReadStream(path)) {
+            resultStream = new ByteArrayInputStream(StreamUtils.copyToByteArray(inputStream));
+        }
+        catch (InvalidFileException e) {
+            throw new IOException(e);
+        }
+
+        String mediaType = tika.detect(resultStream);
+        resultStream.reset();
+
+        if (!StringUtils.hasText(mediaType) || !mediaType.startsWith("image")) throw new NotFoundException();
+
+        if (null != scale) {
+            BufferedImage image = Scalr.resize(
+                ImageIO.read(resultStream),
+                scaleMethod,
+                Scalr.Mode.AUTOMATIC,
+                scale.getWidth(),
+                scale.getHeight()
+            );
+
+            try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+                String subType = mediaType.split("/")[1];
+                ImageIO.write(image, subType, outputStream);
+
+                resultStream.close();
+                resultStream = new ByteArrayInputStream(outputStream.toByteArray());
             }
-            catch (Exception e) {
-                sink.error(e);
-            }
-        });
+        }
+
+        final InputStream finalResultStream = resultStream;
+
+        return ResponseEntity.ok()
+            .contentLength(resultStream.available())
+            .contentType(MediaType.parseMediaType(mediaType))
+            .eTag(id)
+            .body(DataBufferUtils.readInputStream(
+                () -> finalResultStream,
+                new DefaultDataBufferFactory(),
+                (int) fileWebProperties.getBufferSize().toBytes()));
     }
 
-    public Flux<DataBuffer> image(String id, ImageScale scale, ServerHttpResponse response) {
-        return image(id, scale, Scalr.Method.AUTOMATIC, response);
-    }
-
-    public Flux<DataBuffer> image(String id, ImageScale scale, Scalr.Method scaleMethod, ServerHttpResponse response) {
-        String path = getFilePath(id);
-
-        if (!fileSystem.exists(path)) throw new NotFoundException();
-
-        return DataBufferUtils.readInputStream(
-            () -> {
-                InputStream resultStream;
-                try (InputStream inputStream = fileSystem.openReadStream(path)) {
-                    resultStream = new ByteArrayInputStream(StreamUtils.copyToByteArray(inputStream));
-                }
-
-                String mediaType = tika.detect(resultStream);
-                resultStream.reset();
-
-                if (!StringUtils.hasText(mediaType) || !mediaType.startsWith("image")) throw new NotFoundException();
-                response.getHeaders().set("Content-Type", mediaType);
-
-                if (null != scale) {
-                    BufferedImage image = Scalr.resize(
-                        ImageIO.read(resultStream),
-                        scaleMethod,
-                        Scalr.Mode.AUTOMATIC,
-                        scale.getWidth(),
-                        scale.getHeight()
-                    );
-
-                    try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-                        String subType = mediaType.split("/")[1];
-                        ImageIO.write(image, subType, outputStream);
-
-                        resultStream.close();
-                        resultStream = new ByteArrayInputStream(outputStream.toByteArray());
-                    }
-                }
-
-                response.getHeaders().set("Content-Length", String.valueOf(resultStream.available()));
-                return resultStream;
-            },
-            new DefaultDataBufferFactory(),
-            (int) fileWebProperties.getBufferSize().toBytes());
-    }
-
-    private void saveFile(String filename, InputStream data) {
+    protected void saveFile(String filename, InputStream data) {
         try {
             data.reset();
             fileSystem.put(getFilePath(filename), data);
@@ -248,7 +273,7 @@ public class FileWebController {
         }
     }
 
-    public ImageScale imageScale(String scale) {
+    protected ImageScale imageScale(String scale) {
         if (!StringUtils.hasText(scale)) return null;
 
         String[] values = scale.split("X");
